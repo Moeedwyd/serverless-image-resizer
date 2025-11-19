43d1f81679ce0703069ae21a7243b8376e7f13a6
Moeedwyd <khan8540@saskpolytech.ca>
2025-11-18 22:01:42 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const AWS = require("aws-sdk");
+const AWS = require(aws-sdk);
 const s3 = new AWS.S3();
-const sharp = require("sharp");
+const sharp = require(sharp);
 
-exports.handler = async (event) => {
+exports.handler = async (event) = {
     try {
         const bucket = event.bucket;
         const key = event.key;
         const tinyBucket = event.tiny_bucket;
 
+         Get original image
         const image = await s3.getObject({
-            Bucket: bucket,
-            Key: key
+            Bucket bucket,
+            Key key
         }).promise();
 
+         Resize to 200px width
         const resized = await sharp(image.Body)
             .resize(200)
             .toBuffer();
 
-        const newKey = "thumb-" + key;
+        const newKey = thumb- + key;
 
+         Upload resized image
         await s3.putObject({
-            Bucket: tinyBucket,
-            Key: newKey,
-            Body: resized,
-            ContentType: "image/jpeg"
+            Bucket tinyBucket,
+            Key newKey,
+            Body resized,
+            ContentType imagejpeg
         }).promise();
 
         return {
-            status: "success",
-            message: "Image resized",
-            outputKey: newKey
+            status success,
+            message Image resized,
+            outputKey newKey
         };
 
     } catch (err) {
         console.error(err);
         return {
-            status: "fail",
-            error: err.message
+            status fail,
+            error err.message
         };
     }
 };
